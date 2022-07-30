9041aaed579a6389e3f9939514906bd8f034d8c9
jmurillo064 <jmurillo064@gmail.com>
2022-07-30 14:43:17 -0500

    antes de la tragedia II

--- src/app/pages/clasificacion/enferma/enferma.page.ts
+++ src/app/pages/clasificacion/enferma/enferma.page.ts
@@ -3,6 +3,7 @@ import { ImageService } from 'src/app/services/image.service';
 import { SanaService } from 'src/app/services/sana.service';
 import { EnfermaService } from 'src/app/services/enferma.service';
 import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-enferma',
@@ -12,14 +13,17 @@ import { Router } from '@angular/router';
 export class EnfermaPage implements OnInit {
 
   parcelas: any;
+  tratamientos: any;
   imgURL: any;
   estado: any;
+  radioValue;
 
   constructor(
     private sanaService: SanaService,
     private imageService: ImageService,
     private enfermaService: EnfermaService,
-    private router: Router
+    private router: Router,
+    public toastController: ToastController
     ) { }
 
   ngOnInit() {
@@ -41,12 +45,48 @@ export class EnfermaPage implements OnInit {
     }).unsubscribe();
 
     //tratamientos
-    
+    this.enfermaService.obtenerTratamiento().then(data =>{
+      if(data['code'] != "200"){
+        console.log("Sin datos");
+      }else{
+        this.tratamientos = data['data'];
+      }
+    }).catch(error =>{
+      console.log(error);
+    });
   }
+
+  //enferma-routing.module.ts
+  //function to print what is inputed in the form that is declared above
+checkValue(event) { 
+  console.log(event.detail.value);
+
+  for (let a of this.tratamientos) {
+    if (a['tipo_tratamiento']==this.radioValue){
+      console.log(a['descripcion_tratamiento']);
+      this.presentToast(a['descripcion_tratamiento']);
+      break;
+    } 
+  }
+//console.log(this.tratamientos[0]);
+}
+
+print(event) {
+  console.log('Selected value: ', this.radioValue);
+}
 
   //guardar
   guardar(){
-    this.router.navigate(['tabs']);
+    console.log(this.tratamientos[0]);
+    //this.router.navigate(['tabs']);
+  }
+  //mensaje
+  async presentToast(msj) {
+    const toast = await this.toastController.create({
+      message: msj,
+      duration: 2000
+    });
+    toast.present();
   }
 
 }
--- src/app/services/enferma.service.ts
+++ src/app/services/enferma.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EnfermaService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  obtenerTratamiento(){
+    let  url = 'https://shrouded-gorge-69430.herokuapp.com/api/Tratamientos';
+    return new Promise ((resolve, reject) => {
+      this.http.get(url).subscribe(res => {
+        resolve(res);{
+        }
+      }, error => {
+        reject(error);
+      });
+    });
+  }
 }
